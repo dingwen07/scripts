2b6734bdb2c90b84fba25780f9d4f574e2bc12cc
Dingwen Wang <wangdingwen32@gmail.com>
2025-06-19 03:28:20 -0400

    Update rlockosx.sh

--- rlockosx.sh
+++ rlockosx.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Check if exactly one argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $(basename $0) <destination>"
+# Use localhost if no argument is provided
+if [ "$#" -eq 0 ]; then
+    destination="localhost"
+elif [ "$#" -eq 1 ]; then
+    destination="$1"
+else
+    echo "Usage: $(basename "$0") [destination]"
     exit 1
 fi
-
-# Assign the first argument as the host
-destination="$1"
 
 # Use SSH to send the sleep command to the specified host
 ssh "$destination" '/usr/bin/pmset displaysleepnow'
